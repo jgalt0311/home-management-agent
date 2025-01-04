0597b26844d69f1185752a9b13cd3f2d667cf5f9
jgalt0311 <35353489+jgalt0311@users.noreply.github.com>
2025-01-03 14:40:16 -1000

    Initialize project with core files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeManagementAgent from './components/HomeManagementAgent';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <HomeManagementAgent />
+    </div>
+  );
+}
+
+export default App;
